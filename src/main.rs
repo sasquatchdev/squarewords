f034d9e7e8a597497d61d6e88d14afef531e2e37
sasquatchdev <about@sasquatchdev.me>
2025-03-13 21:39:32 +0100

    feat: dfs and words implementation for trie

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 #![allow(unused)]
 
+use model::trie;
+
 mod common;
 
 mod dict;
 mod model;
 
+pub const N: usize = 3;
+
 fn main() {
+    let dict = 
+        dict::from_csv("dictionary.csv".into())
+        .expect("Could not load dictionary.")
+        .into_iter()
+        .filter(|word| word.len() == N)
+        .collect::<Vec<String>>();
     
+    let mut trie = trie::Trie::new();
+    trie.insert_many(dict);
 }
--- src/model/trie.rs
+++ src/model/trie.rs
@@ -5,7 +5,7 @@ pub struct Trie {
     root: Node,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Node {
     children: HashMap<char, Node>,
     is_end: bool
@@ -17,8 +17,13 @@ impl Trie {
             root: Node::default()
         }
     }
+    pub fn from_node(node: Node) -> Self {
+        Trie {
+            root: node
+        }
+    }
 
-    pub fn insert(&mut self, word: &str) {
+    pub fn insert(&mut self, word: String) {
         let mut current = &mut self.root;
 
         for c in word.chars() {
@@ -28,13 +33,13 @@ impl Trie {
         current.is_end = true;
     }
 
-    pub fn insert_many(&mut self, words: Vec<&str>) {
+    pub fn insert_many(&mut self, words: Vec<impl Into<String>>) {
         for word in words {
-            self.insert(word);
+            self.insert(word.into());
         }
     }
 
-    fn traverse(&self, word: &str) -> Option<&Node> {
+    fn traverse(&self, word: String) -> Option<&Node> {
         let mut current = &self.root;
 
         for c in word.chars() {
@@ -48,13 +53,42 @@ impl Trie {
         Some(current)   
     }
 
-    pub fn contains(&self, word: &str) -> bool {
+    pub fn contains(&self, word: String) -> bool {
         let node = self.traverse(word);
         node.is_some() && node.unwrap().is_end
     }
 
-    pub fn contains_partial(&self, word: &str) -> bool {
+    pub fn contains_partial(&self, word: String) -> bool {
         let node = self.traverse(word);
         node.is_some()
+    }
+
+    pub fn words(&self) -> Vec<String> {
+        let mut results = Vec::new();
+        self.dfs(&self.root, "".to_string(), &mut results);
+        results
+    }
+
+    pub fn words_with_prefix(&self, prefix: String) -> Vec<String> {
+        let mut results = Vec::new();
+        let node = match self.traverse(prefix.clone()) {
+            Some(node) => node,
+            None => return results
+        };
+
+        self.dfs(node, prefix, &mut results);
+        results
+    }
+
+    fn dfs(&self, node: &Node, prefix: String, results: &mut Vec<String>) {
+        if node.is_end {
+            results.push(prefix.to_string());
+        }
+
+        for (c, child) in &node.children {
+            let mut prefix = prefix.clone();
+            prefix.push(*c);
+            self.dfs(child, prefix, results);
+        }
     }
 }
