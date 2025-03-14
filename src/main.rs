66fb9d9754b4b28ef631a04c7f0b29e8a23035a0
sasquatchdev <about@sasquatchdev.me>
2025-03-14 13:08:53 +0100

    feat: frequency finder

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,14 @@ mod model;
 mod find;
 
 pub const SIZE: usize = 3;
-pub const TOP_N: u64 = 0;
+pub const TOP_N: u64 = 100;
 
 fn main() {
     let freq =
         dict::freq_from_csv("res/frequency.csv".into())
         .expect("Could not load frequency.")
         .into_iter()
+        .filter(|(_, place)| place <= &TOP_N)
         .collect::<Frequency>();
 
     let dict: Vec<String> = 
@@ -24,6 +25,7 @@ fn main() {
         .expect("Could not load dictionary.")
         .into_iter()
         .filter(|word| word.len() == SIZE)
+        .filter(|word| freq.iter().any(|(f_word, _)| f_word == word))
         .collect::<Dictionary>();
     
     let mut trie = trie::Trie::new();
